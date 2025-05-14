56a17916b1d37b46b0010ac724a415597ef9e37e
Anatoliy <anatoliypuss5@gmail.com>
2025-05-14 13:00:42 +0300

    Third cmmit

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ app.post('/v1/order/create', (req, res) => {
 
 app.post('/v1/webhook', (req, res) => {
     console.log('Webhook endpoint accessed');
+    console.log('Body:', req.body)
+    console.log('Everything', JSON.stringify(req))
     res.json({ status: 'success' });
 });
 
