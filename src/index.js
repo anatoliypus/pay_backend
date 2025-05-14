ef01bc222e51ec7b54c2b73f6aeed70eaa8dc4b2
Anatoliy <anatoliypuss5@gmail.com>
2025-05-14 13:10:54 +0300

    5

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.post('/v1/order/create', (req, res) => {
 app.post('/v1/webhook', (req, res) => {
     console.log('Webhook endpoint accessed');
     console.log('Body:', req.body)
-    console.log('Everything', reg)
+    console.log('Everything', req)
     res.json({ status: 'success' });
 });
 
